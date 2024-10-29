3997dcb7659794e030826f57199039eba852e849
George Zeng <georgezzeng@gmail.com>
2024-10-29 17:09:31 -0500

    for rows that have null values in the institution_name column, they would result in false for the share_institution column because null values are also considered duplicates. Now count the occurrences to assign values

--- src/share_institution.py
+++ src/share_institution.py
@@ -2,5 +2,12 @@
 
 def shared_institution(df):
     """checks whether the institution is shared between facilities"""
-    df["share_institution"] = df.duplicated(subset=["institution_name"], keep=False)
+    df["share_institution"] = False
+
+    # Group by institution_name and count occurrences
+    counts = df.groupby('institution_name')['facility_name'].transform('count')
+
+    # Results in True if count > 1 and institution_name is not null
+    df["share_institution"] = (counts > 1) & df['institution_name'].notna()
+
     return df
