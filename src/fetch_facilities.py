ad08b9fa5dd753054433f282073cb9d432ad5add
George Zeng <georgezzeng@gmail.com>
2024-10-29 15:13:18 -0500

    Return a dict with id as the key and facility name as the value

--- src/fetch_facilities.py
+++ src/fetch_facilities.py
@@ -10,20 +10,19 @@ def extract_insitution_id(yaml_path):
         return data.get("InstitutionID")
 
 def get_facilities(repo="topology"):
-    facilities = []
+    facilities_dict = {}
     for root, dirs, files in os.walk(repo): # Generate the file names in a directory tree by walking the tree either top-down or bottom-up.
         for file in files:
-            if file == "Facility.yaml":
-                yaml_path = os.join(root, file) # construct the file path by joining the root and file name
+            if file == "FACILITY.yaml":
+                yaml_path = os.path.join(root, file) # construct the file path by joining the root and file name
                 institution_id = extract_insitution_id(yaml_path)
 
                 facility_name = os.path.basename(os.path.dirname(yaml_path))
+                facilities_dict[institution_id] = facility_name
 
-                facilities.append({
-                    "FacilityName": facility_name,
-                    "InstitutionID": institution_id
-                })
-    return pd.DataFrame(facilities)
+    print(facilities_dict)
+
+    return facilities_dict
 
 
 
