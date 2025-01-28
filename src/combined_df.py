fdff7672870dc4150b6b57e0d9d1fc52af5d644c
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2025-01-28 16:41:47 -0600

    Add institution_id column

--- src/combined_df.py
+++ src/combined_df.py
@@ -9,7 +9,7 @@ def combine_facilities_and_institutions(facilities_dict, institutions_dict):
         institution_id = facility["institution_id"]
         facility_name = facility["facility_name"]
         institution_name = institutions_dict.get(institution_id)
-        combined_list.append({"facility_name": facility_name, "institution_name": institution_name})
+        combined_list.append({"institution_id": institution_id, "facility_name": facility_name, "institution_name": institution_name})
 
     df = pd.DataFrame(combined_list)
     return df
