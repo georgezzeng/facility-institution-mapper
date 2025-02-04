16f10b3a76e471b5f095d19cf24c7edfa361012c
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2025-02-04 10:38:57 -0600

    grab ror_id and reformat it

--- src/fetch_institutions.py
+++ src/fetch_institutions.py
@@ -5,7 +5,17 @@ def get_institutions(api_url="https://topology-institutions.osg-htc.org/api/inst
     response = requests.get(api_url)
     if response.status_code == 200:
         data = response.json()
-        institutions_dict = {institution['id']: institution['name'] for institution in data}
+        institutions_dict = {}
+
+        for institution in data:
+            institution_id = institution["id"]
+            institution_name = institution["name"]
+            ror_id_url = institution["ror_id"]
+
+            ror_id = ror_id_url.split("/")[-1] if ror_id_url else None
+
+            institutions_dict[institution_id] = {"name": institution_name, "ror_id": ror_id}
+
         print(institutions_dict)
         return institutions_dict
 
